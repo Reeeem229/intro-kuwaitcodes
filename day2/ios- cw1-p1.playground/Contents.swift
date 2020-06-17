501e4abd0f17515ac01c9c729a9ea6474cf8791e
Reeeem229 <66528681+Reeeem229@users.noreply.github.com>
2020-06-17 21:16:11 +0300

    done

---
+++ day2/ios- cw1-p1.playground/Contents.swift
@@ -0,0 +1,10 @@
+let family = ["dalal", "aziz","abullah","dana", "reem" ]
+
+
+var evens = [2, 4, 8, 10, 12]
+
+var grades = [90.0, 87.7, 89.0,]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[4])
