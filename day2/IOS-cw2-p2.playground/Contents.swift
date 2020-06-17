51dae0a76751d3e47698b67cda23b65952d4fd6a
Reeeem229 <66528681+Reeeem229@users.noreply.github.com>
2020-06-17 19:25:26 +0300

    CW2 done

---
+++ day2/IOS-cw2-p2.playground/Contents.swift
@@ -0,0 +1,16 @@
+var fruits = ["mango", "orange", "patato", "peach"]
+    print (fruits[0])
+print(fruits[3])
+
+
+    fruits.append("strawberry")
+print(fruits)
+
+fruits.remove(at: 2 )
+print(fruits)
+
+fruits += ["banana", "watermlon", "pinapple", "kiwi"]
+print(fruits)
+fruits.removeAll()
+print(fruits)
+
